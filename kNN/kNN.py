65ef2b5e9580b852cdde265b9c9d7ae136fc869c
Mukhammad Karimov <karimov8887@gmail.com>
2017-10-09 19:24:36 +0900

    Unittesting

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -19,16 +19,22 @@ def classify(data, dataset, labels, k):
     """
     give label to new data, learning from dataset and labels
     """
+    # step 1. distance calculation
     dataset_size = dataset.shape[0]
-    matrix_difference = tile(data, (dataset_size, 1)) - dataset.any()
+    matrix_difference = tile(data, (dataset_size, 1)) - dataset
     sq_matrix_distance = matrix_difference ** 2
     sq_distance = sq_matrix_distance.sum(axis=1)
     distance = sq_distance ** 0.5
+
     sorted_distance_indicies = distance.argsort()
     class_count = {}
 
-    for i in range (k):
+    # step 2. voting with lowest k distances
+    for i in range(k):
         vote_i_label = labels[sorted_distance_indicies[i]]
         class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
+
+    # step 3. sort dictionary
     sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+
     return sorted_class_count[0][0]
--- kNN/test_knn.py
+++ kNN/test_knn.py
@@ -1,10 +1,26 @@
-"""
-Test knn algorithm
-"""
+
 import knn
+import unittest
 
 
-GROUPS, LABELS = knn.create_dataset()
+class KNNTestCase(unittest.TestCase):
+    """
+    Test knn algorithm
+    """
+    
+    def setUp(self):
+        self.groups, self.labels = knn.create_dataset()
+
+    def test_knn_classify(self):
+        test_point = [0, 0]
+        another_test_point = [1, 0.9]
+
+        classified_label = knn.classify(test_point, self.groups, self.labels, 3)
+        another_classified_label = knn.classify(another_test_point, self.groups, self.labels, 3)
+
+        self.assertEqual(classified_label, 'B')
+        self.assertEqual(another_classified_label, 'A')
+
 
 if __name__ == '__main__':
-    knn.classify([0, 0], GROUPS, LABELS, 3)
+    unittest.main()
